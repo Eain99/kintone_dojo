2244f6ded83edc3c2e32a50001252a40433d122b
ahandsel <genji-fujimori@cybozu.co.jp>
2022-07-01 15:48:38 +0900

    task2.js - Improve comments

--- day2/task2.js
+++ day2/task2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Task2-1:fileManage obj contains the files and their permission settings
+// Task2-1: fileManage obj contains the files and their permission settings
 const fileManage =
 {
   f001: { folderName: "情報システム部", viewAuth: "Everyone", editAuth: "情報システム部", deleteAuth: "情報システム部" },
@@ -9,17 +9,16 @@ const fileManage =
 };
 
 // Task2-2 オブジェクトに格納されている値を全てブラウザに表⽰する
-//  Task2-2: Output the fileManage object in HTML using table's id
+// Task2-2: Output the fileManage object in HTML using table's id
 const file = Object.keys(fileManage);
 
 file.forEach((fileId) => {
-  // console.log('fileId');  // Expected f001,f002,f003
-  // console.log(fileId); 
+  // console.log(fileId); // Expected f001,f002,f003
 
   const innerLoop = Object.keys(fileManage[fileId]);
 
   innerLoop.forEach((fileProperty) => { // loops through files
-    console.log(fileId + '-' + fileProperty);
+    // console.log(fileId + '-' + fileProperty);
     document.getElementById(fileId + '-' + fileProperty).textContent = fileManage[fileId][fileProperty];
 
     // find the files with the view auth 'Everyone'
