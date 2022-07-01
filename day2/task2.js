c6dca150f3a5c720dca35842203a53c2b36d6fa8
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-07-01 14:37:34 +0900

    Renamed the parameters(variables)in task2 javascript file

--- day2/task2.js
+++ day2/task2.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Task2-1:fileManage obj contains the files and their permission settings
 const fileManage =
 {
   f001: { folderName: "情報システム部", viewAuth: "Everyone", editAuth: "情報システム部", deleteAuth: "情報システム部" },
@@ -5,20 +8,28 @@ const fileManage =
   f003: { folderName: "営業部", viewAuth: "Everyone", editAuth: "営業部", deleteAuth: "営業部" }
 };
 
-const keys = Object.keys(fileManage);
+// Task2-2 オブジェクトに格納されている値を全てブラウザに表⽰する
+//  Task2-2: Output the fileManage object in HTML using table's id
+const file = Object.keys(fileManage);
 
-keys.forEach((key) => {
-  //console.log(a);
+file.forEach((fileId) => {
+  // console.log('fileId');  // Expected f001,f002,f003
+  // console.log(fileId); 
 
-  const innerLoop = Object.keys(fileManage[key]);
+  const innerLoop = Object.keys(fileManage[fileId]);
 
-  innerLoop.forEach((key2) => {
-    //console.log(key+'-'+key2);
-    document.getElementById(key + '-' + key2).textContent = fileManage[key][key2];
+  innerLoop.forEach((fileProperty) => { // loops through files
+    console.log(fileId+'-'+fileProperty);
+    document.getElementById(fileId + '-' + fileProperty).textContent = fileManage[fileId][fileProperty];
 
-    if (=== 'Everyone') {
-
+    // find the files with the view auth 'Everyone'
+    if (fileManage.fileId.fileProperty.viewAuth === "Everyone") {
+      console.log(fileManage.fileId.fileProperty.folderName);
     }
+    // display the foldername where viewAuth is 'Everyone'
+
+    // if ( === 'Everyone') {
+    // }
 
   })
 
