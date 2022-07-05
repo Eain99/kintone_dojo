f5b7cd1e9655e9f4eb58d6cc7dcbcc7759cde56a
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-07-05 11:08:30 +0900

    Update task4-1.js

--- day4/task4-1.js
+++ day4/task4-1.js
@@ -1,33 +1,35 @@
 'use strict';
 
-const mainPrice = 3900;
+let mainPrice = 3900;
 const price = document.getElementById('price');
 
+// secureOptionにチェックを入れた時
 document.getElementsByClassName('chk')[0].onclick = () => {
 
 const secureOption = document.getElementsByClassName('chk')[0].checked;
 const diskExpansion= document.getElementsByClassName('chk')[1].checked;
 
 if(secureOption && diskExpansion) {
-mainPrice = 6150;
-price.innerText = mainPrice;
+  mainPrice = 6150;
+  price.innerText = mainPrice;
 }else if(secureOption && !diskExpansion){
-mainPrice = 5150;
-price.innerText = mainPrice;
+  mainPrice = 5150;
+  price.innerText = mainPrice;
 }else if(!secureOption && diskExpansion){
-  mainPrice= 4900;
+  mainPrice = 4900;
   price.innerText = mainPrice;
 }else if (!secureOption && !diskExpansion){
   mainPrice = 3900;
   price.innerText = mainPrice;
 }
 
+//diskExpansionにチェックを入れたとき
 document.getElementsByClassName('chk')[1].onclick= () => {
 
   const secureOption1 = document.getElementsByClassName('chk')[0].checked;
   const diskExpansion1 = document.getElementsByClassName('chk')[1].checked;
 
-  if(secureOption1 && diskExpansion1 === true) {
+  if(secureOption1 && diskExpansion1) {
     mainPrice =6150;
     price.innerText = mainPrice;
   } else if (secureOption1 && !diskExpansion1){
@@ -43,16 +45,8 @@ document.getElementsByClassName('chk')[1].onclick= () => {
 }
 }
 
-//  let mainPrice = new Array();
-// mainPrice[0] = 250;
-// mainPrice[1] = 1000;
+// const standardPrice = 3900;
+// const secureOption = 1250;
+// const diskExpansion = 3000;
+// const price = document.getElementById('price');
 
-// function keisan(){
-//   final = 3900 ;
-//   for(i=3900;i<=1;i++){
-//     if(document.myForm.elements[i].checked == true){
-//       final=final+mainPrice[i];
-//     }
-//   }
-//  document.myForm.total.value=final;
-// }
