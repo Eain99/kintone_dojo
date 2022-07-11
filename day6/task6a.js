b80c07bcd55c978c3c815d887623f4a175f251d5
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-07-11 13:34:30 +0900

    Create task6a.js

---
+++ day6/task6a.js
@@ -0,0 +1,29 @@
+(()=> {
+'use strict';
+kintone.events.on('app.record.create.show', (event) => {
+  console.log(event);
+  let table = event.record.Table;
+
+  table.value =[];
+  const action5 = ['あくなき探求',
+            '不屈の身体',
+             '理想への共感',
+             '心を動かす',
+             '知識を増やす',
+             '公明正大'
+           ];
+
+           action5.forEach((title) => {
+             table.value.push({
+               value :{
+                 Action5: {type:'DROP_DOWN',value:title},
+                 状況:{type:'CHECK_BOX',value:['未振り返り']},
+                 課題:{type:'MULTI_LINE_TEXT',value:''}
+               }
+             });
+           });
+           return event;
+});
+
+
+})();
