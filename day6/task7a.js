7666790a07f85fbd6b3d59f70f9f0b6c6b080fec
Eain99 <eain-lay@cybozu.co.jp>
2022-07-13 22:30:42 +0900

    task7a

--- day6/task7a.js
+++ day6/task7a.js
@@ -1,50 +1,50 @@
 (()=> {
 'use strict';
-// Add create and edit events on the "日付" process
-kintone.events.on(['app.record.create.change.日付','app.record.edit.change.日付'],(event)=>{
-date = event.record.日付.value;
 
+const kEvent =['app.record.create.change.日付','app.record.edit.change.日付','app.record.create.change.サイボウズ製品','app.record.edit.change.サイボウズ製品','app.record.create.change.管理番号','app.record.edit.change.管理番号','app.record.create.change.重複禁止項目＿文字列','app.record.edit.change.重複禁止項目＿文字列'];
 
-});
-// Add create and edit events on the "サイボウズ製品" process
-kintone.events.on(['app.record.create.change.サイボウズ製品','app.record.edit.change.サイボウズ製品'],(event)=> {
-product = event.record.サイボウズ製品.value;
+kintone.events.on(kEvent,(event)=> {
+console.log(event);
 
-// use switch to get the values and display the values on the 重複禁止項目＿文字列 field
-switch(product){
- case 'kintone':
-   product='KN';
-   event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}';
-   break;
- case 'Garoon':
-    product='GR';
-    event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}';
-    break; 
- case 'サイボウズOffice':
-    product='OF';
-    event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}';
-    break;
- case 'Mailwise':
-     product='MW';
-     event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}';
-     break;
+// event.record.重複禁止項目＿文字列.disabled = true;
+// event.record.重複禁止項目＿文字列.value = `${dateId} - ${productType[0]} - ${numberManage}`;
+const dateGet = event.record.日付.value;
+console.log(dateGet);
+
+const dateId=dateFns.format(dateGet,'YYYYMMDD');
+console.log(dateId);
+
+// get values from 管理番号
+const numberManage = event.record.管理番号.value;
+console.log(numberManage);
+// get values from サイボウズ製品
+const productTitle = event.record.サイボウズ製品.value;
+
+const productType =['KN','GR','OF','MW'];
+console.log(productType);
+
+switch(productTitle){
+  case 'kintone':
+  event.record.重複禁止項目_文字列.value =`${dateId} - ${productType[0]} - ${numberManage}`;
+  break;
+  case 'Garoon':
+  event.record.重複禁止項目_文字列.value =`${dateId} - ${productType[1]} - ${numberManage}`;
+  break;
+  case 'サイボウズOffice':
+  event.record.重複禁止項目_文字列.value =`${dateId} - ${productType[2]} - ${numberManage}`;
+  break;
+  case 'Mailwise':
+  event.record.重複禁止項目_文字列.value =`${dateId} - ${productType[3]} - ${numberManage}`;
+  break;
+
+//   const duplicate =event.record.重複禁止項目＿文字列.value;
+//  duplicate.disabled =true;
 }
-return event;
+// event.record.重複禁止項目＿文字列.disabled =true; // prohibit the duplicate
+// event.record.重複禁止項目＿文字列.value = `${dateId} - ${productType[1]} - ${numberManage}`;
 
-});
-
-// Add create and edit events on the "管理番号" process
-kintone.events.on(['app.record.create.change.管理番号','app.record.edit.change.管理番号'],(event)=>{
-number = event.record.管理番号.value; // get the value
-event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}'; // display it on 重複禁止項目＿文字列 field
 return event;
 });
 
-// Add create and edit events on the "重複禁止項目＿文字列" process
-kintone.events.on(['app.record.create.change.重複禁止項目＿文字列','app.record.edit.change.重複禁止項目＿文字列'],(event)=>{
-console.log(event); // display it on console
-event.record.重複禁止項目＿文字列.disabled =true; // prohibit the duplicate
-event.record.重複禁止項目＿文字列.value = '${YYYYMMDD}-${product}-${number}'; // display that format on 重複禁止項目＿文字列 field
-return event;
-})
+
 })();
---
+++ day6/task7aNotes.md
@@ -0,0 +1,34 @@
+# Steps
+
+## Create variable for events 
+
+* keyword - (const)constant .
+
+* create variable(kEvent) and put the events of three process (日付、サイボウズ製品、管理番号)
+
+## Create event 
+
+* in event, Use the variable (kEvent) to make it easier to read
+
+* check the process is going well with console.log
+
+## get the values from 日付 using date-fns library
+
+* create a variable(dateGet) to get value from 日付
+
+* check the dateGet with console.log
+
+* get the date from date-fns library with the format ("YYYYMMDD");
+
+## get the values from 管理番号 
+
+* declare a variable (numberManage) and get values from 管理番号
+
+## get the values from サイボウズ製品 using Array and Switch statement
+
+* create a variable to get values from サイボウズ製品
+
+* create array for the values 
+
+* use switch statement to get the values
+
