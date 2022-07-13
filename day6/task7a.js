9b13d6b9880a7139f4cf50713bc957d6ac2556f3
ahandsel <genji-fujimori@cybozu.co.jp>
2022-07-13 15:09:37 +0900

    Create dateInput variable

--- day6/task7a.js
+++ day6/task7a.js
@@ -2,7 +2,7 @@
   'use strict';
   // Add create and edit events on the "日付" process
   kintone.events.on(['app.record.create.change.日付', 'app.record.edit.change.日付'], (event) => {
-    date = event.record.日付.value;
+    const dateInput = event.record.日付.value;
 
 
   });
