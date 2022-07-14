1f057868ad7353aa5bb721e9b925dcd82f58eaad
ahandsel <genji-fujimori@cybozu.co.jp>
2022-07-14 11:12:57 +0900

    Add lables to console.log output

--- day6/task7a.js
+++ day6/task7a.js
@@ -4,18 +4,22 @@
   const kEvent = ['app.record.create.change.日付', 'app.record.edit.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.edit.change.サイボウズ製品', 'app.record.create.change.管理番号', 'app.record.edit.change.管理番号', 'app.record.create.change.重複禁止項目＿文字列', 'app.record.edit.change.重複禁止項目＿文字列'];
 
   kintone.events.on(kEvent, (event) => {
+    console.log(`event`);
     console.log(event);
 
     //  event.record.重複禁止項目＿文字列.disabled = true;
     //  event.record.重複禁止項目＿文字列.value = `${dateId} - ${productType} - ${numberManage}`;
     const dateGet = event.record.日付.value;
+    console.log(`dateGet`);
     console.log(dateGet);
 
     const dateId = dateFns.format(dateGet, 'YYYYMMDD');
+    console.log(`dateId`);
     console.log(dateId);
 
     // get values from 管理番号
     const numberManage = event.record.管理番号.value;
+    console.log(`numberManage`);
     console.log(numberManage);
 
     // get values from サイボウズ製品
@@ -28,6 +32,7 @@
     productType[1] = "GR";
     productType[2] = "OF";
     productType[3] = "MW";
+    console.log(`productType`);
     console.log(productType);
 
     switch (productTitle) {
@@ -45,6 +50,7 @@
         break;
 
         const duplicate = event.record.重複禁止項目＿文字列.value;
+        console.log(`duplicate`);
         console.log(duplicate);
     }
     // event.record.重複禁止項目＿文字列.disabled =true; // prohibit the duplicate
@@ -54,6 +60,7 @@
   });
 
   // kintone.events.on(['app.record.create.change.重複禁止項目＿文字列','app.record.edit.change.重複禁止項目＿文字列'],(event)=>{
+  // console.l`og(event`);
   // console.log(event);
   // event.record.重複禁止項目＿文字列.disabled =true; // prohibit the duplicate
   // event.record.重複禁止項目＿文字列.value = `${dateId} - ${productType} - ${numberManage}`;
