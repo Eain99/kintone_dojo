62e33d98ac0af5b9e41a05b98d8bf2f5fc234d70
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-07-20 19:09:37 +0900

    In progress of midterm task

---
+++ Midterm/task5.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+axios.get('https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo', {
+  params: {
+    id: 'dojo'
+  }
+})
+.then((resp)=>{
+  console.log(resp);
+const table=document.createElement('table');
+
+resp.data.forEach((key)=>{
+const tr=document.createElement('tr'); // created row
+table.appendChild(tr);
+
+// put data with td ( the data> day,category,url)
+const date=document.createElement('td');
+date.innerHTML=key.day.value;
+tr.appendChild(date); // created date
+console.log(date.innerHTML);
+
+const category=document.createElement('td');
+category.innerHTML=key.category.value;
+tr.appendChild(category);// created category
+console.log(category.innerHTML);
+
+const bColor =key.label.value;
+
+
+// for(i=0;i<bColor.length;i++)
+// {cells=bColor[i].getElementsByTagName ('td');
+// if (cell[0].innerHTML == '')
+// }
+
+ const url = document.createElement('td');
+ url.innerHTML=`<a href=""></a>`;
+})
+
+})
