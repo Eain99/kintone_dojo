2858fde90612a31f967ac5875711fbbc6780fa8e
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-07-21 16:01:13 +0900

    Finished midterm task

--- Midterm/task5.js
+++ Midterm/task5.js
@@ -1,4 +1,4 @@
-
+(()=> {
 'use strict';
 
 axios.get('https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo', {
@@ -6,35 +6,41 @@ axios.get('https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-d
     id: 'dojo'
   }
 })
-.then((resp)=>{
-  console.log(resp);
-const table=document.createElement('table');
+  .then((resp) => {
+    console.log(resp);
+    const table = document.getElementById('CybozuPage')
 
-resp.data.forEach((key)=>{
-const tr=document.createElement('tr'); // created row
-table.appendChild(tr);
+    resp.data.forEach((key) => {
+      const tr = document.createElement('tr'); // created row
+      table.appendChild(tr);
 
-// put data with td ( the data> day,category,url)
-const date=document.createElement('td');
-date.innerHTML=key.day.value;
-tr.appendChild(date); // created date
-console.log(date.innerHTML);
+      // put data with td ( the data> day,category,url)
+      const date = document.createElement('td');
+      date.innerHTML = key.day.value;
+      tr.appendChild(date); // created date
+      // console.log(date.innerHTML);
 
-const category=document.createElement('td');
-category.innerHTML=key.category.value;
-tr.appendChild(category);// created category
-console.log(category.innerHTML);
+      const category = document.createElement('td');
+      category.innerHTML = key.category.value;
+      tr.appendChild(category);// created category
+      category.setAttribute('class',`${key.label.value}`)
+      // console.log(category.innerHTML);
 
-const bColor =key.label.value;
+      // const bColor = key.label.value;
 
 
-// for(i=0;i<bColor.length;i++)
-// {cells=bColor[i].getElementsByTagName ('td');
-// if (cell[0].innerHTML == '')
-// }
+      // for(i=0;i<bColor.length;i++)
+      // {cells=bColor[i].getElementsByTagName ('td');
+      // if (cell[0].innerHTML == '')
+      // }
 
- const url = document.createElement('td');
- url.innerHTML=`<a href=""></a>`;
-})
+      const url = document.createElement('td');
+      url.innerHTML = `<a href= "${key.url.value}" target="${key.target.value}">${key.content.value}</a>`;
+     tr.appendChild(url);
 
-})
+    
+    })
+
+
+  });
+})();
