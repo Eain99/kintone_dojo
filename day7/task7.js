559beee7b9da49792f6761e18471cbf24e6d4c2d
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-08-01 23:07:19 +0900

    correction

--- day7/task7.js
+++ day7/task7.js
@@ -4,21 +4,14 @@
   const kEvent = ['app.record.create.change.日付', 'app.record.edit.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.edit.change.サイボウズ製品', 'app.record.create.change.管理番号', 'app.record.edit.change.管理番号'];
 
   kintone.events.on(kEvent, (event) => {
-    console.log(`event`);
-    console.log(event);
 
     const dateGet = event.record.日付.value;
-    console.log(`dateGet`);
-    console.log(dateGet);
 
     const dateId = dateFns.format(dateGet, 'YYYYMMDD');
-    console.log(`dateId`);
-    console.log(dateId);
-
+  
     // get values from 管理番号
     const numberManage = event.record.管理番号.value;
-    console.log(`numberManage`);
-    console.log(numberManage);
+  
 
     // get values from サイボウズ製品
     const productTitle = event.record.サイボウズ製品.value;
@@ -31,23 +24,30 @@
     productType[2] = "OF";
     productType[3] = "MW";
 
-    console.log(`productType`);
-    console.log(productType);
-
     switch (productTitle) {
-      case 'kintone':
-        event.record.重複禁止項目_文字列.value = `${dateId}-${productType[0]}-${numberManage}`;
-        break;
-      case 'Garoon':
-        event.record.重複禁止項目_文字列.value = `${dateId}-${productType[1]}-${numberManage}`;
-        break;
-      case 'サイボウズOffice':
-        event.record.重複禁止項目_文字列.value = `${dateId}-${productType[2]}-${numberManage}`;
-        break;
-      case 'Mailwise':
-        event.record.重複禁止項目_文字列.value = `${dateId}-${productType[3]}-${numberManage}`;
-        break;
+  
+      
     }
     return event;
   });
+
+  kintone.events.on(['app.record.create.show','app.record.edit.show'],((event)=>{
+ event.record.重複禁止項目_文字列.disabled=true;
+ return event;
+
+  }));
+
 })();
+
+// case 'kintone':
+//   event.record.重複禁止項目_文字列.value = `${dateId}-${productType[0]}-${numberManage}`;
+//   break;
+// case 'Garoon':
+//   event.record.重複禁止項目_文字列.value = `${dateId}-${productType[1]}-${numberManage}`;
+//   break;
+// case 'サイボウズOffice':
+//   event.record.重複禁止項目_文字列.value = `${dateId}-${productType[2]}-${numberManage}`;
+//   break;
+// case 'Mailwise':
+//   event.record.重複禁止項目_文字列.value = `${dateId}-${productType[3]}-${numberManage}`;
+//   break;
