c93cdf7c97e8a33e5ffb01396a0a435ce5a75f31
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-08-02 15:01:23 +0900

    Correction

--- day7/task7.js
+++ day7/task7.js
@@ -8,10 +8,10 @@
     const dateGet = event.record.日付.value;
 
     const dateId = dateFns.format(dateGet, 'YYYYMMDD');
-  
+
     // get values from 管理番号
     const numberManage = event.record.管理番号.value;
-  
+
 
     // get values from サイボウズ製品
     const productTitle = event.record.サイボウズ製品.value;
@@ -24,16 +24,28 @@
     productType[2] = "OF";
     productType[3] = "MW";
 
+    let ind;
     switch (productTitle) {
-  
-      
+      case 'kintone':
+        ind = 0;
+        break;
+      case 'Garoon':
+        ind = 1;
+        break;
+      case 'サイボウズOffice':
+        ind = 2;
+        break;
+      case 'Mailwise':
+        ind = 3;
+        break;
     }
+    event.record.重複禁止項目_文字列.value = `${dateId}-${productType[ind]}-${numberManage}`;
     return event;
   });
 
-  kintone.events.on(['app.record.create.show','app.record.edit.show'],((event)=>{
- event.record.重複禁止項目_文字列.disabled=true;
- return event;
+  kintone.events.on(['app.record.create.show', 'app.record.edit.show'], ((event) => {
+    event.record.重複禁止項目_文字列.disabled = true;
+    return event;
 
   }));
 
