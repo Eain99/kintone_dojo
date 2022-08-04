33e08c57a9da4304459186e1b2bb15e2968ce717
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-08-04 14:27:14 +0900

    処理修正

--- day4/task4-1.js
+++ day4/task4-1.js
@@ -2,31 +2,27 @@
 let standardPrice = 3900;
 const price = document.getElementById('price');
 const checkbox = document.getElementsByClassName('chk');
-console.log(checkbox);
 
 // secureOptionにチェックを入れた時
 checkbox[0].onclick = () => {
 
-// const secureOption = document.getElementsByClassName('chk')[0].checked;
-// const diskExpansion= document.getElementsByClassName('chk')[1].checked;
-
-if(checkbox[0].checked){
-  price.innerText=standardPrice+Number(checkbox[0].value*5);
-}
-
+  if (checkbox[0].checked) {
+    standardPrice += Number(checkbox[0].value) * 5;
+  }
+  else {
+    standardPrice -= Number(checkbox[0].value) * 5;
+  }
+  price.textContent = standardPrice;
 }
 //diskExpansionにチェックを入れたとき
-checkbox[1].onclick= () => {
+checkbox[1].onclick = () => {
 
-if(checkbox[1].checked){
-  price.innerText=standardPrice+Number(checkbox[1].value);
+  if (checkbox[1].checked) {
+    standardPrice += Number(checkbox[1].value);
+  }
+  else {
+    standardPrice -= Number(checkbox[1].value);
+  }
+  price.textContent = standardPrice;
 }
- 
-} // end of disExpansion process
-// end of secureOption process
-
-// const standardPrice = 3900;
-// const secureOption = 1250;
-// const diskExpansion = 3000;
-// const price = document.getElementById('price');
 
