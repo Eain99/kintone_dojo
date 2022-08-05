b9a26a660432f4d7d961c6adce6ccfe83b90e323
Eain99 <106043969+Eain99@users.noreply.github.com>
2022-08-05 13:57:10 +0900

    correction

--- day9/task9.js
+++ day9/task9.js
@@ -1,10 +1,15 @@
 (() => {
   'use strict';
 
-  kintone.events.on('app.record.create.submit', (event) => {
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
     const recordValue = event.record.重複禁止項目.value;
-    const query = '重複禁止項目 = "' + recordValue + '"';
+    const recordId = kintone.app.record.getId();
+    let query = '重複禁止項目 = "' + recordValue + '"';
 
+    if (recordId) {
+      query = '重複禁止項目 = "' + recordValue + '" and レコード番号 != "' + recordId + '"';
+    }
+    
     const params = {
       'app': kintone.app.getId(),
       'query': query
@@ -17,29 +22,6 @@
           return event;
         } else {
           event.error = "キャンセルしました"
-          return event;
-        }
-      }
-    });
-  });
-
-  kintone.events.on('app.record.edit.submit', (event) => {
-    const recordValue = event.record.重複禁止項目.value;
-    const recordValue2 = event.record.レコード番号.value;
-    const query = '重複禁止項目 = "' + recordValue + '" and レコード番号 != "' + recordValue2 + '"';
-
-    const params = {
-      'app': kintone.app.getId(),
-      'query': query
-    }
-    return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params).then((resp) => {
-      if (resp.records.length === 0) {// If it is not duplicating, event will be returned
-        return event;
-      } else {
-        if (window.confirm('レコードが重複しています。そのままで保存しますか。')) {
-          return event;
-        } else {
-          event.error = "cancelled"
           return event;
         }
       }
